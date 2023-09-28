3c42a2efe3ccd8e9aa0a7cf1b0728d0326ba083e
SoumyadipaPathak <psoumyadipa@gmail.com>
2023-09-28 11:53:37 +0530

    1st commit

---
+++ app/Http/Controllers/catagoryController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Models\Catagory;
+use Illuminate\Http\Request;
+
+class catagoryController extends Controller
+{
+    public function index(){
+        return Catagory::all();
+    }
+}
---
+++ app/Models/Catagory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Catagory extends Model
+{
+    use HasFactory;
+    protected $fillable=[
+        'id',
+        'name',
+        'mata_title',
+        'image',
+        'catagory_keywords',
+        'meta_description',
+        'status'
+    ];
+}
